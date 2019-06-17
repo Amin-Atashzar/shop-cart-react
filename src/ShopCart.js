783a7b7280c9964545422e25d760df414c0c4428
Amin-Atashzar <49231593+Amin-Atashzar@users.noreply.github.com>
2019-06-17 17:30:51 +0430

    Replace if statement with short-circuit

--- src/ShopCart.js
+++ src/ShopCart.js
@@ -3,28 +3,30 @@ import ShowTotal from "./ShowTotal";
 
 function ShopCart(props) {
   const showingCart = props.cart.filter(item => item.count > 0);
-  if (showingCart.length === 0) {
-    return (
-      <div className="cart-empty">
-        <div>
-          <p>Click products to buy</p>
-        </div>
-      </div>
-    );
-  }
   return (
-    <div className="cart">
-      {showingCart.map(item => (
-        <div className="cart-item" key={item.id}>
-          <p>Title : {item.title}</p>
-          <p>Price : {item.price}</p>
-          <p>Count : {item.count}</p>
-          <p>total : {item.count * item.price}</p>
-          <button onClick={() => props.onDelete(item.id)}>Delete</button>
+    <React.Fragment>
+      {showingCart.length === 0 && (
+        <div className="cart-empty">
+          <div>
+            <p>Click products to buy</p>
+          </div>
         </div>
-      ))}
-      {<ShowTotal cart={showingCart} />}
-    </div>
+      )}
+      {showingCart.length !== 0 && (
+        <div className="cart">
+          {showingCart.map(item => (
+            <div className="cart-item" key={item.id}>
+              <p>Title : {item.title}</p>
+              <p>Price : {item.price}</p>
+              <p>Count : {item.count}</p>
+              <p>total : {item.count * item.price}</p>
+              <button onClick={() => props.onDelete(item.id)}>Delete</button>
+            </div>
+          ))}
+          {<ShowTotal cart={showingCart} />}
+        </div>
+      )}
+    </React.Fragment>
   );
 }
 
